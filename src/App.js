a034cf62410b0b8d4d00901da5e25138b141a1b7
1amSumit <sj779619@gmail.com>
2023-05-26 09:44:09 +0530

    Router defination added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div></div>;
-}
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+
+const routerDefinations = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductsPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routerDefinations);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
